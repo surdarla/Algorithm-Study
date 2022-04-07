1174ff8b233bcff5319777a4e2f935be604e078e
TJKim <tjkim67@gmail.com>
2022-04-07 20:44:23 +0900

    0407 edited

--- TJ_Kim/(0407)Counting sort1.py
+++ TJ_Kim/(0407)Counting sort1.py
@@ -1,9 +1,9 @@
 def countingSort(arr):
-    result_array = [0 for i in range(len(arr))] # [0]*100 possible
+    result_array = [0]*100 # [0 for i in range(len(arr))] << not working
     for i in arr:
         result_array[i] += 1
         
     return result_array
 
 # 해당 숫자에 해당하는 index 를 1씩 더해주면 됨.
-# arr 크기 제한이 100으로 걸려 있으므로 result_array를 [0]*100 해줘도 무관.
+# [0 for i in range(len(arr))] << arr 크기만큼 [0] 을 만들어주면 안됨. [0 3 5] 이런 방식으로 숫자를 건너띄고 input 을 받는 경우가 있음. 
